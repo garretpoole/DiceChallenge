96b53745821ae30e15d928b03cae26b9bc9dd940
Garret Poole <garretpoole@users.noreply.github.com>
2022-05-14 16:11:10 -0700

    Initial Commit

---
+++ DiceChallenge/DiceChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceChallengeApp.swift
+//  DiceChallenge
+//
+//  Created by Garret Poole on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
